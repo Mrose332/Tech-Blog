4105a0e6e60dc9768860690264f3a105f20f8321
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-24 17:47:23 -0400

    updated post route

--- controllers/api/commentroutes.js
+++ controllers/api/commentroutes.js
@@ -1,23 +1,34 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const {User,Post,Comment
+} = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-// Create comment based upon the comment form
-router.post('/', withAuth, async (req, res) => {
- 
-    try {
-      const post = await Comment.create({
-        comment: req.body.comment,
-        user_id: req.session.user_id,
-        post_id: req.body.post_id,
-        
-      });
-      console.log(req.body);
-      res.status(200).json(post);
-    } catch (err) {
-      res.status(400).json(err);
+//Get all comments
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Create a comment
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.session.user_id
+            })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     }
-  });
+});
+
 
 module.exports = router;
