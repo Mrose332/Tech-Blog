d973976f85710c559c9b6eeeb58d8ea4ec16686c
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-30 16:41:00 -0400

    updated index.js routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeroutes.js');
-const dashboardRoutes = require('./dashboardRoutes.js');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashbaordRoutes');
 
 
 router.use('/api', apiRoutes);
